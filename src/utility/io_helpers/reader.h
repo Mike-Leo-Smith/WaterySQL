6c694d2f96c3be1a10fcf19de83ab42a06d41bc9
mike <mango@live.cn>
2018-12-15 16:59:32 +0800

    added file reader

---
+++ src/utility/io_helpers/reader.h
@@ -0,0 +1,31 @@
+//
+// Created by Mike Smith on 2018-12-15.
+//
+
+#ifndef WATERYSQL_FILE_READER_H
+#define WATERYSQL_FILE_READER_H
+
+#include <string>
+#include <string_view>
+#include <filesystem>
+#include <fstream>
+
+#include "../type_constraints/non_trivial_constructible.h"
+
+namespace watery {
+
+struct FileReader : NonTrivialConstructible {
+    
+    static std::string_view read(std::string_view file_name) {
+        thread_local static std::string buffer;
+        auto size = std::filesystem::file_size(file_name);
+        buffer.resize(size + 1);
+        std::ifstream{file_name}.read(buffer.data(), size);
+        return buffer;
+    }
+    
+};
+
+}
+
+#endif  // WATERYSQL_FILE_READER_H
