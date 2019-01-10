eb462b2a99080f569dad7e0a67279393fb46f177
mike <mango@live.cn>
2019-01-10 14:46:55 +0800

    added doc

---
+++ test/dataset/create_and_insert.sql
@@ -0,0 +1,5 @@
+source "/Users/mike/Desktop/数据库/WaterySQL/res/dataset/create.sql";
+source "/Users/mike/Desktop/数据库/WaterySQL/res/dataset/customer.sql";
+source "/Users/mike/Desktop/数据库/WaterySQL/res/dataset/restaurant.sql";
+source "/Users/mike/Desktop/数据库/WaterySQL/res/dataset/food.sql";
+source "/Users/mike/Desktop/数据库/WaterySQL/res/dataset/orders.sql";
